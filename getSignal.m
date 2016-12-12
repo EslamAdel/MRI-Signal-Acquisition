04cb8f0445392b696ee2bf2096eabc879585f4f3
EslamAdel <eslam.adel.mahmoud.ali@gmail.com>
2016-12-12 16:35:27 +0200

    Working 1D simulation TODO: GUI

--- getSignal.m
+++ getSignal.m
@@ -1,4 +1,4 @@
-function signal = getSignal(protons, Gx, FOVx, tau, phaseAcc, T2)
+function [signal, time] = getSignal(protons, Gx, FOVx, tau, phaseAcc, T2, dt)
 %getSignal Summary of this function goes here
 % Gx in mT/m
 % FOVx in mm.
@@ -12,26 +12,27 @@ rotatedProtons = protons;
 x = -1*FOVx/2:Rx:FOVx/2-Rx; %in mm 
 
 %% Rotation matrix due to Gx 
-gamma = 42.6; %MHz/T
+gamma = 2*pi*42.6; %MHz/T
 
 % w = gamma * x
 w = gamma*Gx.*x;
 
 %% Get dt equation 5.77
+if(dt == 0)
 dt = 2*pi/(Gx*gamma*FOVx);
-
+end
 %% if no pahse accumulation so start aquisition
 if phaseAcc == 0
-time = 0:dt:tau;
+time = 0:dt:tau-dt;
 signal = zeros(1,length(time));
 idx = 1;
-for t = 0:dt:tau
+for t = 0:dt:tau-dt
     angles = w*t;
     for i = 1:size(protons, 2)
         rotatedProtons(:,i) = rotateVector(protons(:,i), angles(i), 'Z');
     end
     totalM = sum(rotatedProtons, 2)*exp(-t/T2);
-    signal(idx) = norm(totalM);
+    signal(idx) = totalM(1) + totalM(2)*1j;
     idx = idx + 1;
 end
 
--- mainScript.m
+++ mainScript.m
@@ -1,13 +1,20 @@
+clc;
+clear;
+close all;
 protons = [1 0 0]';
 protons = repmat(protons,1,200);
 FOVx = 20; %mm
 Gx = 10; %mT/m
-tau = 1; %sec
-rotatedProtons = getSignal(protons, Gx, FOVx, tau,1, 0.4);
-signal = getSignal(rotatedProtons, Gx, FOVx, tau, 0, 0.4);
-t = 0:tau/length(signal):tau - tau/length(signal);
+tau = 0.5;%sec
+dt = 0.001; %sec
+rotatedProtons = getSignal(protons, -1*Gx, FOVx, tau,1, 1000,0);
+[signalFFT, t] = getSignal(rotatedProtons, Gx, FOVx, tau, 0, 1000,dt);
 figure,
-plot(t,signal);
-signalFFT = fftshift(signal);
-figure, plot(abs(signalFFT));
-figure, plot(angle(signalFFT));
+plot(t,real(signalFFT),'b','linewidth',1.5);
+grid on;
+title('The Magnitude of Signal S(t)');
+signal = fft(signalFFT);
+figure,
+plot(abs(circshift(signal', round(length(signal)/2))),'b','linewidth',1.5);
+grid on;
+title('Reconstructed Signal');
