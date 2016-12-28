3be828df46edc90f5727674a4cef642d5080b6d8
eslamadel <eslam.adel.mahmoud.ali@gmail.com>
2016-12-29 01:15:22 +0200

    updating mainScript

--- mainScript.m
+++ mainScript.m
@@ -1,39 +1,36 @@
- clc;
- clear all;
- close all;
+clc;
+clear all;
+close all;
+  
+Gx = 10e-3;   
+objectWidth = 20e-3;  
+Rx = 5e-4;  
+protons = [1 0 0]';
+protons = repmat(protons,1,round(objectWidth/Rx));
+tau = 5e-3;
+Accumulate = 1; 
+dt = 0.1e-4;
+  
+T2 = 100;  
+  
+[signalFFT, t] = getSignal(protons, Gx, objectWidth, Rx, tau, Accumulate, T2,dt);
+  
+figure,
+plot(t,real(signalFFT),'b','linewidth',1.5);
+xlabel('Time (sec)');
+ylabel('Mangnitude');
+grid on;
+title('The Magnitude of Signal S(t)');
  
- %% Setting parameters
- Gx = 10e-3;  % in T/m
- FOVx = 20e-3;  % in m
- Rx = 5e-4; % in m
- protons = [1 0 0]';
- protons = repmat(protons,1,round(FOVx/Rx));
- tau = 5e-3;  %sec
- Accumulate = 1; 
- dt = 0.1e-4; %sec
- fs = 1/dt;
- 
- %% Ignore T2 Effect 
- T2 = 100;  %sec
- 
- %% Start Simulation
- [signalFFT, t] = getSignal(protons, Gx, FOVx, Rx, tau, Accumulate, T2,dt);
- 
- %% Plot the sampled k-space 
- figure,
- plot(t,real(signalFFT),'b','linewidth',1.5);
- xlabel('Time (sec)');
- ylabel('Mangnitude');
- grid on;
- title('The Magnitude of Signal S(t)');
- 
- %% Plot Reconstructed Signal from sampled k-space
  signal = fftshift(ifft(signalFFT));
- w = [-length(t)/2+1:length(t)/2]*2*pi*fs/length(t);
- figure,
- plot(w, abs(signal),'b','linewidth',1.5);
- xlabel('Frequency w (rad/sec)');
+ len = length(t);
+ gamma = 2*pi*42.58*10^6;
+ FOVx = 2*pi/(Gx*gamma*dt);
+ x = [-FOVx/2+FOVx/len:FOVx/len:FOVx/2]*(10^3);
+ figure;
+ plot(x, abs(signal),'b','linewidth',1.5);
+ xlabel('X (mm)');
  ylabel('Magnitude');
- axis([-fs/2 fs/2]);
+ axis([min(x)-2 max(x)+2 0 max(abs(signal))+0.2]);
  grid on;
  title('Reconstructed Signal');
